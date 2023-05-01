682b0dbf05b9d41dcc0da838bc6c3cf76d43047a
Simon Ayers <8039906+simonayers@users.noreply.github.com>
2023-05-01 01:31:22 +0100

    Added base command type to deal with Wimp_Poll dragging end events.

--- !SALib/SALib/salib/mousecommand.h
+++ !SALib/SALib/salib/mousecommand.h
@@ -3,6 +3,8 @@
 #ifndef SALIB_WIMP_MOUSECOMMAND_H
 #define SALIB_WIMP_MOUSECOMMAND_H
 
+#include "rectangle.h"
+
 namespace SALib {
 
 namespace Wimp {
@@ -14,10 +16,17 @@ namespace Wimp {
       virtual void ProcessClick(const int screenXPos, const int screenYPos,
                                 const int button, const int iconHandle) const = 0;
    };
-}
+
+   class DraggingEndedCommandBase {
+   public:
+      virtual ~DraggingEndedCommandBase(void) {}
+
+      virtual void Process(const Rectangle dragBox) const = 0;
+   };
 
 }
 
+}
 
 #endif  // SALIB_WIMP_MOUSECOMMAND_H
 
