e5f746bf18b84939cfda0eb30d590820ff0e468f
Simon Ayers <8039906+simonayers@users.noreply.github.com>
2023-05-05 10:28:51 +0100

    Added some support for text icons.

--- !SALib/SALib/icon.cc
+++ !SALib/SALib/icon.cc
@@ -4,6 +4,7 @@
 #include "oslib/wimp.h"
 #include "salib/icon.h"
 #include "salib/iconbuilder.h"
+#include "salib/reporter.h"
 #include "salib/rectangle.h"
 #include "salib/window.h"
 
@@ -11,7 +12,11 @@ namespace SALib {
 
 namespace Wimp {
 
-static int Icon_CreateIcon(const IconBuilder& iconBuilder, const Window& window)
+Icon::Icon(const IconBuilder& iconBuilder, const Window& window)
+   : m_window(window),
+     m_iconHandle(0),
+     m_spriteName(iconBuilder.GetIconDataBuilder().GetSpriteName()),
+     m_text(iconBuilder.GetIconDataBuilder().GetText())
 {
    wimp_icon_create iconBlock;
 
@@ -22,25 +27,24 @@ static int Icon_CreateIcon(const IconBuilder& iconBuilder, const Window& window)
    iconBlock.icon.extent.x1 = iconBuilder.GetExtent().xPos1;
    iconBlock.icon.extent.y1 = iconBuilder.GetExtent().yPos1;
 
-// Just going to worry about sprite icons for the moment.
+// Just going to worry about sprite icons and text icons for the moment.
    iconBlock.icon.flags = static_cast<wimp_icon_flags>(iconBuilder.GetIconFlags());
 
-   if (iconBuilder.GetIconDataBuilder().GetSpriteName().size() > 0) {   // Icon has a sprite
-      if (iconBuilder.GetIconDataBuilder().GetSpriteName().size() <= osspriteop_NAME_LIMIT) {
-         std::strncpy(iconBlock.icon.data.sprite, iconBuilder.GetIconDataBuilder().GetSpriteName().c_str(), osspriteop_NAME_LIMIT);
+   if (m_spriteName.size() > 0) {   // Icon has a sprite
+      if (m_spriteName.size() <= osspriteop_NAME_LIMIT) {   
+         std::strncpy(iconBlock.icon.data.sprite, m_spriteName.c_str(), osspriteop_NAME_LIMIT);
       } else {
          // Indirect
       }
+   } else if (m_text.size() > 0) {   // Icon is text only
+      iconBlock.icon.flags |= wimp_ICON_INDIRECTED;  // Force using indirected text
+
+      iconBlock.icon.data.indirected_text.text = reinterpret_cast<char*>(&m_text[0]);
+      iconBlock.icon.data.indirected_text.size = m_text.size() + 1;
+      iconBlock.icon.data.indirected_text.validation = const_cast<char*>("");
    }
 
-   return wimp_create_icon(&iconBlock);
-}
-
-Icon::Icon(const IconBuilder& iconBuilder, const Window& window)
-   : m_window(window),
-     m_iconHandle(Icon_CreateIcon(iconBuilder, window))
-{
-
+   *const_cast<int*>(reinterpret_cast<const int*>(&m_iconHandle)) = wimp_create_icon(&iconBlock);
 }
 
 Icon::~Icon(void)
