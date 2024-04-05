61ac7a2f048cd46c585e0d1718362ef2a11d5915
Simon Ayers <8039906+simonayers@users.noreply.github.com>
2024-04-05 17:11:11 +0100

    Added simple struct to represent a point.

---
+++ !SALib/SALib/salib/point.h
@@ -0,0 +1,19 @@
+// Class to define a point
+
+#ifndef SALIB_WIMP_POINT_H
+#define SALIB_WIMP_POINT_H
+
+namespace SALib {
+
+namespace Wimp {
+
+struct Point {
+   int xPos;
+   int yPos;
+};
+
+}
+
+}
+
+#endif // SALIB_WIMP_POINT_H
